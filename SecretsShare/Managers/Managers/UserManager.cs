1fd0786870fa5166d64490d862c5d9161af255e6
Danil <donul.mukhametshin.03@gmail.com>
2022-12-16 02:27:36 +0500

    Added logic for registering and authorizing a user using a jwt token

--- SecretsShare/Managers/Managers/UserManager.cs
+++ SecretsShare/Managers/Managers/UserManager.cs
@@ -1,9 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using SecretsShare.Managers.ManagersInterfaces;
 using SecretsShare.HelperObject;
 using SecretsShare.Models;
 using SecretsShare.Repositories.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SecretsShare.DTO;
 
 namespace SecretsShare.Managers.Managers
 {
@@ -11,30 +19,70 @@ namespace SecretsShare.Managers.Managers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-        public UserManager(IUserRepository userRepository)
+        public UserManager(IUserRepository userRepository, IConfiguration configuration, IMapper mapper)
         {
             _userRepository = userRepository;
+            _configuration = configuration;
+            _mapper = mapper;
         }
         
         public AuthenticateResponse Authenticate(AuthModel model)
         {
-            throw new System.NotImplementedException();
+            var user = _userRepository
+                .GetAll()
+                .FirstOrDefault(x => x.Email.Value == model.Email && x.Password.Value == model.Password);
+
+            if (user == null)
+            {
+                // todo: need to add logger
+                return null;
+            }
+
+            var token = GenerateJwtToken(user);
+            var refreshToken = GenerateJwtToken(user);
+            user.RefreshToken = refreshToken;
+            var result = _userRepository.UpdateRefreshToken(user, refreshToken);
+            return new AuthenticateResponse(user, token, refreshToken);
         }
 
-        public async Task<AuthenticateResponse> Register(AuthModel userModel)
+        public async Task<AuthenticateResponse> Register(AuthModel model)
         {
-            // userModel.RefreshToken = GenerateJwtToken(userModel);
-            // var addedUser = await _userRepository.Add(userModel);
-            //
-            // var response = Authenticate(new AuthModel
-            // {
-            //     Email = userModel.Email,
-            //     Password = userModel.Password
-            // });
-            //
-            // return response;
-            throw new System.NotImplementedException();
+            var userModel = _mapper.Map<User>(model);
+            userModel.RefreshToken = GenerateJwtToken(userModel);
+            var addedUser = await _userRepository.Add(userModel);
+            
+            var response = Authenticate(new AuthModel
+            {
+                Email = userModel.Email.Value,
+                Password = userModel.Password.Value
+            });
+            
+            return response;
+        }
+        
+        private string GenerateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Secret"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("UserId", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(30),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public AuthenticateResponse UpdateTokens(User entity, string refreshToken)
+        {
+            entity.RefreshToken = GenerateJwtToken(entity);
+            var response = new AuthenticateResponse(entity, GenerateJwtToken(entity), refreshToken);
+            var result = _userRepository.UpdateRefreshToken(entity, refreshToken);
+            return response;
         }
     }
 }
