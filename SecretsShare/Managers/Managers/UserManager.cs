eb65ca7e5396a697f37e4d72be81e676ff56206f
Danil <donul.mukhametshin.03@gmail.com>
2022-12-16 01:46:32 +0500

    Added mapping files, filled in for the user

--- SecretsShare/Managers/Managers/UserManager.cs
+++ SecretsShare/Managers/Managers/UserManager.cs
@@ -1,19 +1,39 @@
 ﻿using System.Threading.Tasks;
+using AutoMapper;
 using SecretsShare.Managers.ManagersInterfaces;
 using SecretsShare.HelperObject;
 using SecretsShare.Models;
+using SecretsShare.Repositories.Interfaces;
 
 namespace SecretsShare.Managers.Managers
 {
     public class UserManager: IUserManager
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        
         public AuthenticateResponse Authenticate(AuthModel model)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<AuthenticateResponse> Register(AuthModel userModel)
+        public async Task<AuthenticateResponse> Register(AuthModel userModel)
         {
+            // userModel.RefreshToken = GenerateJwtToken(userModel);
+            // var addedUser = await _userRepository.Add(userModel);
+            //
+            // var response = Authenticate(new AuthModel
+            // {
+            //     Email = userModel.Email,
+            //     Password = userModel.Password
+            // });
+            //
+            // return response;
             throw new System.NotImplementedException();
         }
     }
