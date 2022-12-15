f6ce57abf9f011b852fcc5e795ce4779747c4f31
Danil <donul.mukhametshin.03@gmail.com>
2022-12-16 01:46:53 +0500

    ProfilingFiles

---
+++ SecretsShare/Profiles/FileProfile.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+
+namespace SecretsShare.Profiles
+{
+    public class FileProfile: Profile
+    {
+
+    }
+}
---
+++ SecretsShare/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SecretsShare.DTO;
+using SecretsShare.Models;
+
+namespace SecretsShare.Profiles
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<AuthModel, User>()
+                .ForMember(dst => dst.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dst => dst.Password, opt => opt.MapFrom(src => src.Password));
+        }
+    }
+}
