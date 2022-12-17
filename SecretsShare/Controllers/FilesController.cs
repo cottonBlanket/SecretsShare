def2ddfab5ec6b105b6112048e9863d9f88494ec
Danil <donul.mukhametshin.03@gmail.com>
2022-12-17 08:26:37 +0500

    Added empty controllers for working with files

---
+++ SecretsShare/Controllers/FilesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecretsShare.Models;
+
+//UploadFile
+//DownLoad File
+//UploadText
+//ViewTextFile
+
+//DownloadModel
+/*
+ * UserId
+ * URL
+ */
+namespace SecretsShare.Controllers
+{
+    [ApiController]
+    [Route("Files")]
+    public class FilesController: ControllerBase
+    {
+        [HttpPost("uploadFile")]
+        public IActionResult UploadFile([FromQuery]UploadFileModel model, IFormFile file)
+        {
+            return Ok();//uri
+        }
+        
+        [HttpGet("downloadFile")]
+        public FileStreamResult DownloadFile([FromQuery]Guid userId, [FromQuery] Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost("uploadText")]
+        public IActionResult UploadText([FromQuery] UploadFileModel model, [FromBody] string text)
+        {
+            return Ok();//uri
+        }
+        
+        [HttpGet("downloadText")]
+        public FileStreamResult ViewTextFile([FromQuery]Guid userId, [FromQuery]Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SecretsShare/Models/UploadModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SecretsShare.Models
+{
+    public class UploadFileModel
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public bool Cascade { get; set; }
+    }
+}
