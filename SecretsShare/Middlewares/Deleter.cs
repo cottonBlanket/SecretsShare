69a274d33a704832e19690a866468c6b09e5b382
Danil <donul.mukhametshin.03@gmail.com>
2022-12-23 12:03:08 +0500

    added deleter

---
+++ SecretsShare/Middlewares/Deleter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using SecretsShare.Managers.ManagersInterfaces;
+
+namespace SecretsShare.Middlewares
+{
+    public class Deleter
+    {
+        private readonly RequestDelegate _next;
+    
+        public Deleter(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IFilesManager filesManager)
+        {
+            await _next.Invoke(context);
+            if (context.Response.Headers.ContainsKey("IsDownload"))
+            {
+                var id = context.Request.Headers[":path"].ToString().Split('=').Last();
+                filesManager.DeleteFile(Guid.Parse(id));
+            }
+        }
+    }
+}
